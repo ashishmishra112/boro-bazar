85091a2c5d67d06b47c6448c4c6ce2219d726fa8
ashishmishra112 <am3913570@gmail.com>
2026-04-03 10:10:44 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,58 @@
+import { MdOutlineLocalPhone } from "react-icons/md";
+import { MdOutlineFax } from "react-icons/md";
+import { MdOutlineEmail } from "react-icons/md";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+   <>
+   <div className="relative border bg-linear-to-r from-blue-200 via-pink-50 to-blue-200  px-6 lg:p-10 min-h-screen">
+      <div className=" grid grid-cols-1 lg:grid-cols-2 pt-10 lg:p-10 place-items-center">
+        <div className="border-none  lg:rounded-l-4xl text-center lg:text-left bg-white p-6 lg:p-10 lg:pr-55 lg:ml-58 flex flex-col gap-2 lg:gap-4 lg:h-140">
+          <h2 className="font-montserrat text-2xl lg:text-4xl font-bold">Get in <span className="text-[#dd5471]">Touch</span> </h2>
+          <span className="font-montserrat text-sm lg:text-base">Contact us by filling out the form below <br/> Thankyou</span>
+          <form action="" className="font-montserrat flex flex-col gap-4 lg:gap-4">
+            <input className="border border-gray-200 pl-6 p-2 w-80 lg:w-100" type="text" id="name" placeholder="Name*" required/>
+            <input className="border border-gray-200 pl-6 p-2 w-80 lg:w-100" type="email" id="email" placeholder="Email"  required/>
+            <input className="border border-gray-200 pl-6 p-2 w-80 lg:w-100" type="tel" id="ph" placeholder="Phone number*"  required/>
+            
+            {/* <input className="border border-gray-200 pl-6 w-100 text-black" type="text" id="option" placeholder="How did you find us?" required/> */}
+            <select className="border border-gray-200 pl-6 p-2 w-80 lg:w-100 text-black" required >
+            <option value="clue">How did you find us?</option>
+            <option value="instagram">Twitter(X)</option>
+            <option value="instagram">Instagram</option>
+            <option value="facebook">Facebook</option>
+            </select>
+            <button type="submit" className=" border bg-[#dd5471] text-white p-2 w-80 lg:w-100">Send</button>
+            <div className="font-montserrat flex gap-10 lg:gap-14 justify-center">
+              <div className="flex flex-col justify-center items-center">
+                <span className="text-xs lg:text-base font-semibold">PHONE</span>
+                <MdOutlineLocalPhone className="w-10 h-6 lg:w-20 lg:h-7"/>
+                <p className="text-xs lg:text-base text-[#dd5471]">9129889006</p>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <span className="text-xs lg:text-base font-semibold">FAX</span>
+                <MdOutlineFax className="w-10 h-6 lg:w-20 lg:h-8"/>
+                <p className="text-xs lg:text-base text-[#dd5471]">9129889006</p>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <span className="text-xs lg:text-base font-semibold">EMAIL</span>
+                <MdOutlineEmail className="w-10 h-6 lg:w-20 lg:h-8"/>
+                <p className="text-xs lg:text-base text-[#dd5471]">info@gmail.com</p>
+              </div>
+            </div>
+          </form>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="lg:absolute lg:left-165 w-90 h-75 md:h-95 lg:h-120 lg:w-110 p-4 lg:p-10">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1754.2757374872476!2d77.1030103050649!3d28.43278779077166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1f54f16ced07%3A0xb22fdbe424a0242b!2sSector%2054%20Chowk!5e0!3m2!1sen!2sin!4v1773394716577!5m2!1sen!2sin" className="w-full h-full border-none rounded-2xl lg:rounded-none" style={{ border: 0 }} loading="lazy"></iframe>
+        </div> 
+        <div className="hidden lg:block border-none lg:rounded-r-4xl bg-[#183a4a] p-6 px-34 lg:p-50 lg:h-140 lg:mr-20">
+
         </div>
-      </main>
-    </div>
+      </div>
+   </div>
+
+   
+   </> 
   );
 }
